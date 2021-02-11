b107c41a1e680c2bbf7d08faea001a4450e3b3a6
bgibz <brendanjamesgibson@gmail.com>
2021-02-10 23:43:41 -0800

    added some analysis for teams playing tired opponents

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def check_dates(_list, num_games, date_range):
     return False
 
 
-def plot_wins_data(data, all_games, season, gd):
+def plot_wins_data(data, all_games, season, gd, folder, palette="RdYlGn"):
     my_dict = {
         "Team": data.team_name,
         "Games": data.gameCount,
@@ -58,8 +58,8 @@ def plot_wins_data(data, all_games, season, gd):
     all_games[data.team_name] = my_dict
     df = pd.DataFrame(my_dict, index=[1])
     file = data.team_name + '.png'
-    filepath = Path.cwd() / "Graphs" / season / gd / file
-    sns.barplot(data=df).set_title(data.team_name)
+    filepath = Path.cwd() / "Graphs" / season / folder / gd / file
+    sns.barplot(data=df, palette=palette).set_title(data.team_name)
     plt.savefig(filepath)
     plt.clf()
     return all_games
@@ -86,35 +86,82 @@ def find_tired_games(season, num_games, num_days, from_zone=tz.gettz('UTC'), to_
     return results
 
 
-def crunch_season_data(season, num_games, num_days):
+def find_tired_opponents(season, num_games, num_days, from_zone=tz.gettz('UTC'), to_zone=tz.gettz('America/New_York')):
+    results = {}
+    buckets = {}
+    for index, row in season.iterrows():
+        raw_date = datetime.strptime(row.Date, "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=from_zone)
+        row.Date = raw_date.astimezone(to_zone).date()
+        buckets[row.Away] = buckets.get(row.Away, [])
+        buckets[row.Home] = buckets.get(row.Home, [])
+        add_game(buckets[row.Away], row, num_games)
+        add_game(buckets[row.Home], row, num_games)
+        away_tired = check_dates(buckets[row.Away], num_games, num_days)
+        home_tired = check_dates(buckets[row.Home], num_games, num_days)
+        if away_tired and home_tired:
+            pass
+        elif away_tired and not home_tired:
+            home_data = results.get(row.Home, condensedGameTeamData.CondensedGameTeamData(row.Home))
+            home_data.update(row)
+            results[row.Home] = home_data
+        elif home_tired and not away_tired:
+            away_data = results.get(row.Away, condensedGameTeamData.CondensedGameTeamData(row.Away))
+            away_data.update(row)
+            results[row.Away] = away_data
+    return results
+
+
+def process_tired_games(season, num_games, num_days):
     games_in_days = str(num_games) + 'in' + str(num_days)
     szn = load_season(season)
     extract = find_tired_games(szn, num_games, num_days)
     szn_tired_games = {}
     for data in extract:
-        szn_tired_games = plot_wins_data(extract[data], szn_tired_games, season, games_in_days)
+        szn_tired_games = plot_wins_data(extract[data], szn_tired_games, season, games_in_days, folder="tired")
     szn_data = pd.DataFrame.from_dict(szn_tired_games, orient='index')
     szn_data.sort_values(by=["Games"], inplace=True)
     # Plot stuff
     sns.color_palette("magma", as_cmap=True)
     szn_plot = sns.barplot(y="Team", x="Games", data=szn_data)
     name = season + ".png"
-    filepath = Path.cwd() / "Graphs" / season / games_in_days / name
+    filepath = Path.cwd() / "Graphs" / season / "tired" / games_in_days / name
     plt.tight_layout()
     plt.savefig(filepath)
     plt.clf()
     # Save a csv
     name = season + ".csv"
-    filepath = Path.cwd() / "Graphs" / season / games_in_days / name
+    filepath = Path.cwd() / "Graphs" / season / "tired" / games_in_days / name
     szn_data.to_csv(filepath)
 
+
+def process_tired_opponents(season, num_games, num_days):
+    games_in_days = str(num_games) + 'in' + str(num_days)
+    szn = load_season(season)
+    extract = find_tired_opponents(szn, num_games, num_days)
+    szn_tired_games = {}
+    for data in extract:
+        szn_tired_games = plot_wins_data(extract[data], szn_tired_games, season, games_in_days,
+                                         folder="tired_opponents", palette="gist_stern")
+    szn_data = pd.DataFrame.from_dict(szn_tired_games, orient='index')
+    szn_data.sort_values(by=["Games"], inplace=True)
+    # Plot stuff
+    szn_plot = sns.barplot(y="Team", x="Games", data=szn_data, palette="mako")
+    name = season + ".png"
+    filepath = Path.cwd() / "Graphs" / season / "tired_opponents" / games_in_days / name
+    plt.tight_layout()
+    plt.savefig(filepath)
+    plt.clf()
+    # Save a csv
+    name = season + ".csv"
+    filepath = Path.cwd() / "Graphs" / season / "tired_opponents" / games_in_days / name
+    szn_data.to_csv(filepath)
 
 # 3 in 4 from 2018-19 season
 season = "201819"
 num_games = 2
 num_days = 2
 
-crunch_season_data(season, num_games, num_days)
+process_tired_games(season, num_games, num_days)
 
 
 print("Done.")
